8be0ccb1b7632347c11256dc5088845389c763c0
Oier <oz.irakasle@gmail.com>
2020-11-22 23:31:05 +0100

    Completado

--- app/src/main/java/com/example/animacion_01/MainActivity.java
+++ app/src/main/java/com/example/animacion_01/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.animacion_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -16,19 +21,54 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-	ImageView o_ImageView;
+	ImageView o_BalonDeArriba;
+	ImageView o_BalonDeAbajo;
+	ImageView o_FormaAnimada;
+	ImageView o_VectorAnimado;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		o_ImageView = findViewById(R.id.balon_imageView);
+		o_BalonDeArriba = findViewById(R.id.BalonDeArriba_imageView);
+		o_BalonDeAbajo = findViewById(R.id.BalonDeAbajoimageView);
+		o_FormaAnimada = findViewById(R.id.formaAnimada_imageView);
+		o_VectorAnimado = findViewById(R.id.vector_animado);
+
 
 		//o_ImageView.setOnClickListener(this::mover);
-		o_ImageView.setOnClickListener(this::mover_ObjectAnimator_2);
+		o_BalonDeArriba.setOnClickListener(this::mover_ViewProperty);
+		o_BalonDeAbajo.setOnClickListener(this::mover_2_Elementos);
+		o_FormaAnimada.setOnClickListener(this::mover_formaAnimada);
+		o_VectorAnimado.setOnClickListener(this::mover_vectorAnimado);
 	}
 
-	public void mover_Animation(View v) {
+	public void mover_vectorAnimado(View v) {
+		((AnimatedVectorDrawable) o_VectorAnimado.getDrawable()).start();
+	}
+	public void mover_formaAnimada(View v) {
+		((AnimationDrawable) o_FormaAnimada.getDrawable()).start();
+	}
+
+	public void mover_ViewProperty(View v) {
+		ViewPropertyAnimator oAnimation = o_BalonDeArriba.animate();
+		oAnimation.alpha(0f).scaleX(2f).scaleY(2f).rotation(100f);
+		oAnimation.setDuration(2000);
+		oAnimation.start();
+	}
+
+	public void mover_2_Elementos(View v) {
+		float fFin     = 700;
+		ObjectAnimator oArribaAnimator = ObjectAnimator.ofFloat(o_BalonDeArriba, "translationX", fFin);
+		ObjectAnimator oAbajoAnimator = ObjectAnimator.ofFloat(o_BalonDeAbajo, "translationX", fFin);
+		oArribaAnimator.setDuration(2000L);
+		oAbajoAnimator.setDuration(2000L);
+		oAbajoAnimator.setStartDelay(500L);
+		AnimatorSet oAnimatorSet = new AnimatorSet();
+		oAnimatorSet.playSequentially(oArribaAnimator, oAbajoAnimator);
+		oAnimatorSet.start();
+	}
+/*	public void mover_Animation(View v) {
 		Animation oAnimacion = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.mover_derecha_y_rotar);
 		o_ImageView.startAnimation(oAnimacion);
 	}
@@ -70,6 +110,6 @@ public class MainActivity extends AppCompatActivity {
 		oObjectAnimator.setDuration(2000L);
 		oObjectAnimator.setStartDelay(500L);
 		oObjectAnimator.start();
-	}
+	}*/
 
 }
